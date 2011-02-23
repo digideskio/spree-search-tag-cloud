68ecdca211ce180002300517c6cdfc405961937b
Steph Skardal <steph@endpoint.com>
2011-02-23 11:21:21 -0500

    Updating search filtering to allow numerics.

--- app/models/search_record.rb
+++ app/models/search_record.rb
@@ -1,5 +1,5 @@
 class SearchRecord < ActiveRecord::Base
   def self.filter(term)
-    term.gsub(/\+/, ' ').gsub(/^\s+/, '').gsub(/\s+$/, '').downcase.gsub(/[^a-z\s-]/, '')
+    term.gsub(/\+/, ' ').gsub(/^\s+/, '').gsub(/\s+$/, '').downcase.gsub(/[^0-9a-z\s-]/, '')
   end
 end
